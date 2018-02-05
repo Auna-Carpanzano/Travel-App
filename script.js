00acd1bf8dd305f85b43f1b46e031eeb380f8927
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-04 18:46:52 -0700

    Add for loop for slides.length that sets slides to display: none

--- script.js
+++ script.js
@@ -27,4 +27,7 @@ function showSlides(n) {
   var unselected = document.getElementsByClassName("unselected");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 }
