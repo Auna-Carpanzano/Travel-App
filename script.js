77e71eea6c1e59b3e321cab1e3305056812da7b9
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-04 18:33:29 -0700

    On input for slider1 call function that changes textContent of output to be the current value

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 var slider1 = document.getElementById("range1");
 var output = document.getElementById("display1");
 output.textContent = slider1.value;
+
+slider1.oninput = function() {
+    output.textContent = this.value;
+}
