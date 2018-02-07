cae98efd9b84186711a9b07967f3a8e58bf56fa5
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-06 21:33:03 -0700

    Add if statement that sets maxHeight of panel to null if maxHeight is true

--- script.js
+++ script.js
@@ -46,6 +46,8 @@ for (i = 0; i < acc.length; i++) {
       this.classList.toggle("active");
 
       var panel = this.nextElementSibling;
-
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      }
   });
 }
