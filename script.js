83f1877c93acdf027151b9ab956e75b92e94fb7e
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-06 21:31:58 -0700

    Add var panel that is the next element sibling of button

--- script.js
+++ script.js
@@ -44,5 +44,8 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
       this.classList.toggle("active");
+
+      var panel = this.nextElementSibling;
+
   });
 }
